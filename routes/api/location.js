e13e50d40b722f7fedf5d5589e70a300a3fb8b87
Jon Bascos <jon@jonbascos.com>
2020-05-28 23:07:10 -0700

    Started working on the Location route

--- routes/api/location.js
+++ routes/api/location.js
@@ -15,7 +15,11 @@ router.get(
     auth,
     [
       check("nameOfLocation", "Location name is required").not().isEmpty(),
-      check(),
+      check("street", "Please enter a street address").not().isEmpty(),
+      check("city", "Please enter a city").not().isEmpty(),
+      check("state", "Please enter in a state").not().isEmpty(),
+      check("zipcode", "Please enter your zipcode").not().isEmpty(),
+      check("text", "Please enter in your text").not().isEmpty(),
     ],
   ],
   async (req, res) => {}
